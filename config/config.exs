445494b9b117806ae24357d05b56758dd52b3b59
kamranhossain <kamran.hossain227@gmail.com>
2020-07-30 23:56:55 +0600

    add config for pow

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Use pow for user authentication
+config :pow_auth_guild, :pow,
+  user: PowAuthGuild.Users.User,
+  repo: PowAuthGuild.Repo
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
