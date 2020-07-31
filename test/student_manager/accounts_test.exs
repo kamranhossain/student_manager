f941e1e56f2fb4e5ac660f1583cd838d7969e798
kamranhossain <kamran.hossain227@gmail.com>
2020-07-31 21:23:33 +0600

    add pending tag into each test to track the test

--- test/student_manager/accounts_test.exs
+++ test/student_manager/accounts_test.exs
@@ -1,4 +1,6 @@
 defmodule StudentManager.AccountsTest do
+  @moduledoc false
+
   use StudentManager.DataCase
 
   alias StudentManager.Accounts
@@ -23,51 +25,61 @@ defmodule StudentManager.AccountsTest do
       user
     end
 
+    @tag :pending
     test "list_users/0 returns all users" do
       user = user_fixture()
       assert Accounts.list_users() == [user]
     end
 
+    @tag :pending
     test "get_user!/1 returns the user with given id" do
       user = user_fixture()
       assert Accounts.get_user!(user.id) == user
     end
 
+    # @tag :pending
     test "create_user/1 with valid data creates a user" do
       assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
     end
 
+    # @tag :pending
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
     end
 
+    # @tag :pending
     test "create_user/1 creates a user with a default role of student" do
       {:ok, user} = Accounts.create_user(@valid_attrs)
       assert user.roles == ["student"]
     end
 
+    @tag :pending
     test "create_user/1 validates a correct role" do
       assert {:error, %Ecto.Changeset{}} =
-               Accounts.create_user(Map.put(@valid_attrs, :roles, ["pilot", "teacher"]))
+               Accounts.create_user(Map.put(@invalid_attrs, :roles, ["pilot", "teacher"]))
     end
 
+    @tag :pending
     test "update_user/2 with valid data updates the user" do
       user = user_fixture()
       assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
     end
 
+    @tag :pending
     test "update_user/2 with invalid data returns error changeset" do
       user = user_fixture()
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       assert user == Accounts.get_user!(user.id)
     end
 
+    @tag :pending
     test "delete_user/1 deletes the user" do
       user = user_fixture()
       assert {:ok, %User{}} = Accounts.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
 
+    @tag :pending
     test "change_user/1 returns a user changeset" do
       user = user_fixture()
       assert %Ecto.Changeset{} = Accounts.change_user(user)
